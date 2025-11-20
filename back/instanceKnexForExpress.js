94e069287e8ebfc484d27d15ac5581eadc710c0e
youkoOOKAWA <youko.ookawa@mail.toyota-body.co.jp>
2025-11-20 09:46:10 +0900

    e

--- back/instanceKnexForExpress.js
+++ back/instanceKnexForExpress.js
@@ -1,7 +1,7 @@
 const config = require("./knexfile");
 const knex = require("knex");
 const envSwitch = process.env.NODE.ENV || "development";
-const db = knex(config.environment);
+const db = knex(config[envSwith]);
 // const productionDB = knex(config.production);
 
 // module.exports = db ? db : productionDB;
