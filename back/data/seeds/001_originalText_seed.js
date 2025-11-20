ef1297b9fa50372d0a373d5458b18e5c597504af
youkoOOKAWA <youko.ookawa@mail.toyota-body.co.jp>
2025-11-20 19:17:25 +0900

    f

--- back/data/seeds/001_originalText_seed.js
+++ back/data/seeds/001_originalText_seed.js
@@ -3,10 +3,10 @@
  * @returns { Promise<void> }
  */
 //原文を持ってくる処理
-const original = require("../data/originaltext.json");
+const original = require("/data/originaltext.json");
 //マイグレで作ったテーブルに豆乳
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("originaltext").del();
-  await knex("originaltext").insert(textsJson);
+  await knex("originaltext").insert(original);
 };
